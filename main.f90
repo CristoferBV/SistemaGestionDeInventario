086342ce2f1d53737867425e2d728255838b0802
CristoferBV <70454042+CristoferBV@users.noreply.github.com>
2023-09-09 19:37:01 -0600

    Update VenderProducto()

--- main.f90
+++ main.f90
@@ -1,6 +1,6 @@
 program SistemaInventario
-    use, intrinsic :: ISO_FORTRAN_ENV, ONLY: ERROR_UNIT
-    implicit none
+    use, intrinsic :: ISO_FORTRAN_ENV, ONLY: ERROR_UNIT !ERROR_UNIT = Esto se utiliza para definir la unidad de error estándar en Fortran.
+    implicit none                                       !módulo ISO_FORTRAN_ENV
 
     integer :: opcion
     character(25) :: nombre
@@ -15,15 +15,17 @@ program SistemaInventario
             case (1)
                 call RegistrarProductos()
             case (2)
-                call VenderProducto(nombre, cantidad_vendida)
+                call VenderProducto()
             case (3)
                 call ConsultarPorNombre(nombre)
             case (4)
                 call ConsultarPorPrecio(precio)
             case (5)
-                EXIT
+                call MostrarInventario()
+            case (6)
+                exit
             case default
-                call ManejarError(1) ! Opción inválida
+                call ManejarError(1)
         end select
     end do
 
@@ -37,7 +39,8 @@ contains
         print *, "2. Vender producto"
         print *, "3. Consultar por nombre"
         print *, "4. Consultar por precio"
-        print *, "5. Salir"
+        print *, "5. Mostrar inventario"
+        print *, "6. Salir"
         print *, ""
         print *, "Seleccione una opcion: "
     end subroutine MostrarMenu
@@ -89,11 +92,76 @@ contains
         call System("CLS")
     end subroutine RegistrarProductos
 
-    subroutine VenderProducto(nombre, cantidad_vendida)
-        character(25) :: nombre
-        integer :: cantidad_vendida
-        ! Implementar la lógica para vender un producto
+    subroutine VenderProducto()
+        implicit none
+        character(50) :: producto_eliminar
+        integer :: num_productos, i
+        logical :: encontrado
+        character(50) :: nombre_producto
+        real :: precio_producto
+        integer :: cantidad_producto
+        character(100) :: file_path
+        file_path = "../SistemaGestionDeInventario/Inventario/inventario.txt"
+
+        ! Abrir el archivo de inventario
+        open(20, file=file_path, status='old')
+
+        ! Inicializar la bandera encontrado
+        encontrado = .false.
+
+        call system("CLS")
+        ! Mostrar el encabezado del inventario
+        print *, "Inventario:"
+        print *, "-----------------------------------------------------------------------------------"
+        print *, "Nombre                                            |       Cantidad   |    Precio"
+        print *, "-----------------------------------------------------------------------------------"
+
+        ! Leer los productos del archivo
+        num_productos = 0
+        do
+            read(20, *, end=10) nombre_producto, cantidad_producto, precio_producto
+            print *, nombre_producto, "|", cantidad_producto, "     |", precio_producto
+            num_productos = num_productos + 1
+        end do
+
+        10  continue
+
+        ! Pedir al usuario que elija un producto para eliminar
+        print *, ""
+        print *, "Ingrese el nombre del producto que desea vender:"
+        read *, producto_eliminar
+
+        ! Buscar el producto en el inventario
+        rewind(20)
+        do i = 1, num_productos
+            read(20, *) nombre_producto, cantidad_producto, precio_producto
+            if (trim(nombre_producto) == trim(producto_eliminar)) then
+                encontrado = .true.
+                exit
+            end if
+        end do
+
+        ! Cerrar el archivo de inventario
+        close(20)
+
+        ! Si se encontró el producto, eliminarlo y actualizar el archivo
+        if (encontrado) then
+            open(20, file=file_path, status='replace')
+            rewind(20)
+            do i = 1, num_productos
+                read(20, *) nombre_producto, cantidad_producto, precio_producto
+                if (trim(nombre_producto) /= trim(producto_eliminar)) then
+                    write(20, *) nombre_producto, cantidad_producto, precio_producto
+                end if
+            end do
+            close(20)
+            print *, "Producto vendido con éxito."
+        else
+            print *, "Producto no encontrado en el inventario."
+        end if
+
     end subroutine VenderProducto
+
 
     subroutine ConsultarPorNombre(nombre)
         character(25) :: nombre
@@ -105,12 +173,52 @@ contains
         ! Implementar la lógica para consultar por precio
     end subroutine ConsultarPorPrecio
 
+    subroutine MostrarInventario()
+        character(25) :: nombre_producto
+        integer :: cantidad_inventario, iostat
+        real :: precio_producto
+        character(100) :: file_path
+        file_path = "../SistemaGestionDeInventario/Inventario/inventario.txt"
+
+        ! Abrir el archivo de inventario en modo lectura
+        open(20, file=file_path, status="old")
+
+        ! Verificar si hubo errores al abrir el archivo
+        if (iostat /= 0) then
+            write(*,*) "Error al abrir el archivo de inventario."
+            stop
+        end if
+
+        call System("CLS")
+        ! Mostrar encabezado de la lista de inventario
+        print *, "    Inventario de Productos:"
+        print *, "---------------------------------"
+        print *, "Nombre     Cantidad    Precio"
+        print *, "---------------------------------"
+
+        ! Leer y mostrar los productos del inventario
+        do
+            read(20, *, iostat=iostat) nombre_producto, cantidad_inventario, precio_producto
+            if (iostat /= 0) exit ! Fin del archivo
+            print *, trim(nombre_producto), cantidad_inventario, precio_producto
+        end do
+
+        ! Cerrar el archivo
+        close(20)
+        call system("PAUSE")
+        call system("CLS")
+    end subroutine MostrarInventario
+
     subroutine ManejarError(codigo)
         integer :: codigo
         select case(codigo)
             case (1)
-                print *, "Opcion invalida. Por favor, seleccione una opción valida."
-            ! Agregar más casos para manejar otros errores
+                print *, ""
+                call system("CLS")
+                print *, "[ERROR]: Por favor, seleccione una opcion valida."
+                print *, ""
+                call system("PAUSE")
+                call system("CLS")
         end select
     end subroutine ManejarError
 
